bcd38019a79fe3dbe6b7da225f7318849ec6c5dc
Ye Naing Tun <yenaingtun.mtkn@gmail.com>
2023-06-28 09:48:25 +0630

    updated

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let speakButton = document.getElementById("play-button");
 let pauseButton = document.getElementById("pause-button");
 let resumeButton = document.getElementById("resume-button");
 let stopButton = document.getElementById("stop-button");
+let speedButton = document.getElementById("speed-button");
 let arr = window.speechSynthesis.getVoices();
 let options = "";
 let selected = "";
@@ -10,19 +11,31 @@ let selected = "";
 arr.map((op, i) => {
     options += `<option value="${op}" id="${i}"">${op.name}</option>`;
 })
-console.log(arr);
 document.getElementById("voices").innerHTML = options;
 const selectOption = document.querySelector("#voices");
-
+let currentChar;
 let text = textArea.value;
 let utterance = new SpeechSynthesisUtterance();
 utterance.text = text;
 utterance.voice = window.speechSynthesis.getVoices()[selectOption.selectedIndex];
-// utterance.lang = "fr-FR";
+utterance.rate = speedButton.value || 1;
+utterance.volume = 2;
+utterance.lang = "en-US";
+utterance.addEventListener('boundary', e => {
+    currentChar = e.charIndex;
+})
 
+speedButton.addEventListener('input', () => {
+    console.log('here');
+    window.speechSynthesis.cancel();
+});
 speakButton.addEventListener("click", function () {
     if ( 'speechSynthesis' in window ) {
         window.speechSynthesis.cancel();
+        utterance.addEventListener('end', () => {
+           textArea.disabled = false;
+        });
+        textArea.disabled = true;
         window.speechSynthesis.speak(utterance);
         document.getElementById('play-button').style.display = 'none';
         document.getElementById('pause-button').style.display = 'block';
@@ -33,9 +46,11 @@ speakButton.addEventListener("click", function () {
 
 pauseButton.addEventListener("click", function () {
     if ( 'speechSynthesis' in window ) {
-        window.speechSynthesis.pause();
-        document.getElementById('pause-button').style.display = 'none';
-        document.getElementById('resume-button').style.display = 'block';
+        if (window.speechSynthesis.speaking) {
+            window.speechSynthesis.pause();
+            document.getElementById('pause-button').style.display = 'none';
+            document.getElementById('resume-button').style.display = 'block';
+        }
     } else {
         alert('not working..');
     }
@@ -43,9 +58,11 @@ pauseButton.addEventListener("click", function () {
 
 resumeButton.addEventListener("click", function () {
     if ( 'speechSynthesis' in window ) {
-        window.speechSynthesis.resume();
-        document.getElementById('pause-button').style.display = 'block';
-        document.getElementById('resume-button').style.display = 'none';
+        if (window.speechSynthesis.pause && window.speechSynthesis.speaking){
+            window.speechSynthesis.resume();
+            document.getElementById('pause-button').style.display = 'block';
+            document.getElementById('resume-button').style.display = 'none';
+        }
     } else {
         alert('not working...');
     }
