6f67bc2ab10b4fcf30513e8c585eaf2e2540bfcf
Ye Naing Tun <yenaingtun.mtkn@gmail.com>
2023-06-28 15:24:58 +0630

    updated speed rate

--- app.js
+++ app.js
@@ -13,59 +13,62 @@ arr.map((op, i) => {
 })
 document.getElementById("voices").innerHTML = options;
 const selectOption = document.querySelector("#voices");
-let currentChar;
+let currentChar = '';
 let text = textArea.value;
 let utterance = new SpeechSynthesisUtterance();
 utterance.text = text;
-utterance.voice = window.speechSynthesis.getVoices()[selectOption.selectedIndex];
+utterance.voice = window.speechSynthesis.getVoices()[selectOption.selectedIndex] || window.speechSynthesis.getVoices()[1];
 utterance.rate = speedButton.value || 1;
 utterance.volume = 2;
 utterance.lang = "en-US";
 utterance.addEventListener('boundary', e => {
     currentChar = e.charIndex;
 })
+utterance.addEventListener('end', () => {
+    textArea.disabled = false
+})
 
-speedButton.addEventListener('input', () => {
-    console.log('here');
-    window.speechSynthesis.cancel();
-});
 speakButton.addEventListener("click", function () {
-    if ( 'speechSynthesis' in window ) {
-        window.speechSynthesis.cancel();
-        utterance.addEventListener('end', () => {
-           textArea.disabled = false;
-        });
-        textArea.disabled = true;
-        window.speechSynthesis.speak(utterance);
-        document.getElementById('play-button').style.display = 'none';
-        document.getElementById('pause-button').style.display = 'block';
-    } else {
-        alert('not working on ur browser!');
-    }
+    speak(textArea.value)
+});
+pauseButton.addEventListener("click", pauseText);
+speedButton.addEventListener('input', () => {
+    window.speechSynthesis.cancel();
+    speak(utterance.text.substring(currentChar));
 });
 
-pauseButton.addEventListener("click", function () {
-    if ( 'speechSynthesis' in window ) {
-        if (window.speechSynthesis.speaking) {
-            window.speechSynthesis.pause();
-            document.getElementById('pause-button').style.display = 'none';
-            document.getElementById('resume-button').style.display = 'block';
-        }
-    } else {
-        alert('not working..');
+function speak(text) {
+    if (speechSynthesis.paused && speechSynthesis.speaking) {
+        return speechSynthesis.resume();
     }
-});
+    utterance.text = text
+    utterance.rate = speedButton.value || 1
+    // if (speechSynthesis.speaking) return
+    textArea.disabled = true
+    speechSynthesis.speak(utterance)
+    document.getElementById('play-button').style.display = 'none';
+    document.getElementById('pause-button').style.display = 'block';
+}
+
+function stopText() {
+    window.speechSynthesis.pause();
+    window.speechSynthesis.cancel();
+}
+
+function pauseText() {
+    if (speechSynthesis.speaking) speechSynthesis.pause()
+    document.getElementById('pause-button').style.display = 'none';
+    document.getElementById('resume-button').style.display = 'block';
+}
+
 
 resumeButton.addEventListener("click", function () {
-    if ( 'speechSynthesis' in window ) {
-        if (window.speechSynthesis.pause && window.speechSynthesis.speaking){
-            window.speechSynthesis.resume();
-            document.getElementById('pause-button').style.display = 'block';
-            document.getElementById('resume-button').style.display = 'none';
-        }
-    } else {
-        alert('not working...');
+    if (window.speechSynthesis.pause && window.speechSynthesis.speaking){
+        window.speechSynthesis.resume();
+        document.getElementById('pause-button').style.display = 'block';
+        document.getElementById('resume-button').style.display = 'none';
     }
+    window.speechSynthesis.resume();
 });
 
 stopButton.addEventListener("click", function () {
